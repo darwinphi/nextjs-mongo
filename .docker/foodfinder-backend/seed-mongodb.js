85506b48db57ad2de1c7183a539b2da5491d6ca5
Darwin Manalo <dar@healthnow.ph>
2024-05-15 21:09:16 +0800

    Initial setup with mongo

---
+++ .docker/foodfinder-backend/seed-mongodb.js
@@ -0,0 +1,23 @@
+db.locations.insert([
+  {
+    address: "6220 Avenue U",
+    zipcode: "NY 11234",
+    borough: "Brooklyn",
+    cuisine: "Cafe",
+    grade: "A",
+    name: "The Roasted Bean",
+    on_wishlist: [],
+    location_id: "56018",
+  },
+
+  {
+    address: "405 Lexington Avenue",
+    zipcode: "NY 10174",
+    borough: "Manhattan",
+    cuisine: "American",
+    grade: "A",
+    name: "The Diner At The Corner",
+    on_wishlist: [],
+    location_id: "63426",
+  },
+]);
