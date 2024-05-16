71191856c457b6c9878d3d62bc69ee7538e38e6f
Darwin Manalo <dar@healthnow.ph>
2024-05-16 16:17:11 +0800

    Add authentication

--- components/locations-details/index.tsx
+++ components/locations-details/index.tsx
@@ -1,12 +1,63 @@
 import { LocationType } from "mongoose/locations/schema";
 import styles from "./index.module.css";
 
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import Button from "components/button";
+
 interface PropsInterface {
   location: LocationType;
 }
 
+interface WishlistInterface {
+  locationId: string;
+  userId: string;
+}
+
 const LocationDetail = (props: PropsInterface): JSX.Element => {
   let location = props.location;
+  const { data: session } = useSession();
+  const [onWishlist, setOnWishlist] = useState<Boolean>(false);
+  const [loading, setLoading] = useState<Boolean>(false);
+
+  useEffect(() => {
+    let userId = session?.user.fdlst_private_userId;
+    setOnWishlist(
+      userId && location.on_wishlist.includes(userId) ? true : false
+    );
+  }, [session, location.on_wishlist]);
+
+  const wishlistAction = (props: WishlistInterface) => {
+    const { locationId, userId } = props;
+    if (loading) {
+      return false;
+    }
+    setLoading(true);
+    let action = !onWishlist ? "addWishlist" : "removeWishlist";
+    fetch("/api/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `mutation wishlist {
+                    ${action}(
+                        location_id: "${locationId}",
+                        user_id: "${userId}"
+                      ){
+                        on_wishlist
+                        } }`,
+      }),
+    })
+      .then((result) => {
+        if (result.status === 200) {
+          setOnWishlist(action === "addWishlist" ? true : false);
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   return (
     <div>
       {location && (
@@ -32,6 +83,22 @@ const LocationDetail = (props: PropsInterface): JSX.Element => {
             {location.grade}
           </li>
         </ul>
+      )}
+
+      {session?.user.fdlst_private_userId && (
+        <Button
+          variant={!onWishlist ? "outline" : "blue"}
+          disabled={loading ? true : false}
+          clickHandler={() =>
+            wishlistAction({
+              locationId: session?.user.fdlst_private_userId,
+              userId: session?.user?.userId,
+            })
+          }
+        >
+          {onWishlist && <>Remove from your Wishlist</>}
+          {!onWishlist && <>Add to your Wishlist</>}
+        </Button>
       )}
     </div>
   );
